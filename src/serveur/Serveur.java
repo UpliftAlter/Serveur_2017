09d63d404438b39cf9d61c12d50ff76620354018
FabienE <fabien.espanol@laposte.net>
2018-01-10 13:59:40 +0100

    FONCTIONS DB SERVEUR

--- src/serveur/Serveur.java
+++ src/serveur/Serveur.java
@@ -98,115 +98,11 @@ public class Serveur {
 	}
 
 	private void initAllGroups() {
-		String url = "jdbc:mysql://localhost:3306/base_de_donnees_neocampus?autoReconnect=true&useSSL=false";
-		String username = "root";
-		String mdp = "root";
-		Connection connexion = null;
-
-		try {
-			connexion = DriverManager.getConnection(url, username, mdp);
-
-			Statement statement = connexion.createStatement();
-			ResultSet resultat1 = statement
-					.executeQuery("SELECT * FROM GROUPE");
-
-			while (resultat1.next()) {
-				int numGroupe = resultat1.getInt("ID_GROUPE");
-				String nomGroupe = resultat1.getString("NOM_GROUPE");
-				Groupe g = new Groupe(nomGroupe, numGroupe);
-				this.allGroups.add(g);
-			}
-
-			for (Groupe groupe : allGroups) {
-
-				int numGroupe = groupe.getIdGroupe();
-
-				String req = "SELECT * FROM UTILISATEUR WHERE ID_UTILISATEUR IN (SELECT ID_UTILISATEUR FROM APPARTENIR WHERE ID_GROUPE = "
-						+ numGroupe + ")";
-				System.out.println(req);
-				ResultSet resultat2 = statement.executeQuery(req);
-				while (resultat2.next()) {
-					int id_utilisateur = resultat2.getInt("ID_UTILISATEUR");
-					String identifiant = resultat2.getString("IDENTIFIANT");
-
-					String mdp2 = resultat2.getString("MOT_DE_PASSE");
-
-					String nom = resultat2.getString("NOM_UTILISATEUR");
-					String prenom = resultat2.getString("PRENOM_UTILISATEUR");
-					String type = resultat2.getString("TYPE_UTILISATEUR");
-
-					Utilisateur u = null;
-					switch (type) {
-					case "ETUDIANT":
-						u = new Etudiant(nom, prenom, mdp2, identifiant,
-								id_utilisateur);
-						break;
-
-					default:
-						break;
-					}
-
-					groupe.addMember(u);
-
-					System.out.println(u);
-				}
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			if (connexion != null)
-				try {
-					/* Fermeture de la connexion */
-					connexion.close();
-				} catch (SQLException ignore) {
-					ignore.printStackTrace();
-				}
-		}
+		allGroups = (ArrayList<Groupe>) database.getAllGroupsBD();
 	}
 
 	private void initAllUsers() {
-		String url = "jdbc:mysql://localhost:3306/base_de_donnees_neocampus?autoReconnect=true&useSSL=false";
-		String username = "root";
-		String mdp = "root";
-		Connection connexion = null;
-
-		try {
-			connexion = DriverManager.getConnection(url, username, mdp);
-
-			Statement statement = connexion.createStatement();
-			ResultSet resultat1 = statement
-					.executeQuery("SELECT * FROM Utilisateur");
-
-			while (resultat1.next()) {
-				int numGroupe = resultat1.getInt("ID_UTILISATEUR");
-				String type = resultat1.getString("TYPE_UTILISATEUR");
-				String nom = resultat1.getString("NOM_UTILISATEUR");
-				String prenom = resultat1.getString("PRENOM_UTILISATEUR");
-
-				Utilisateur u = null;
-				switch (type) {
-				case "ETUDIANT":
-					u = new Etudiant(nom, prenom);
-					break;
-				// ALED TOTO FAUT PAS OUBLIE DE FAIRE LE RESTE
-				default:
-					break;
-				}
-				this.allUsers.add(u);
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		} finally {
-			if (connexion != null)
-				try {
-					/* Fermeture de la connexion */
-					connexion.close();
-				} catch (SQLException ignore) {
-					ignore.printStackTrace();
-				}
-		}
+		database.getAllGroupsBD();
 	}
 
 }
